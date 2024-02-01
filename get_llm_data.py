151dc5c4f16af1e33ad98864e37067a541230852
unknown <a5f23t3e@gmail.com>
2024-02-01 17:38:55 +0800

    fix

---
+++ get_llm_data.py
@@ -0,0 +1,6 @@
+from LLM.helper import *
+
+
+# make sure instruction_induction/ folder exists (downloaded from https://github.com/keirp/automatic_prompt_engineer/tree/main/experiments/data/instruction_induction)
+# (Download the entire folder to this directory!)
+generate_all_data()
